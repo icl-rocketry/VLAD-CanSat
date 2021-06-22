79bea5fc926afffdefe081dcbcebe74a43dc1673
Palash Mattoo <palashmattoo@gmail.com>
2021-06-22 19:52:10 +0100

    Added code snippets to populate later
    Need to first learn how the code actually works in C++ and also figure out a way to externally test it.

--- Firmware/src/children/errorHandling.h
+++ Firmware/src/children/errorHandling.h
@@ -3,4 +3,40 @@
 // provide data to other children
 // Palash
 
-// This is a test to check if the git thing works lmao
+// This snippet will be called any time there is an error update from any of the children. Not usable rn.
+class ErrorHandler
+{
+private:
+    /* data */
+public:
+    ErrorHandler(/* args */);
+    ~ErrorHandler();
+};
+
+ErrorHandler::ErrorHandler(/* args */)
+{
+}
+
+ErrorHandler::~ErrorHandler()
+{
+}
+
+// This snippet will be used by this code itself to buzz any time a (critical) error is spotted. Not usable.
+class BuzzMe
+{
+private:
+    /* data */
+public:
+    BuzzMe(/* args */);
+    ~BuzzMe();
+};
+
+BuzzMe::BuzzMe(/* args */)
+{
+}
+
+BuzzMe::~BuzzMe()
+{
+}
+
+// Not sure what to do about the sending data to other children.
