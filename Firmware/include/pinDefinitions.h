b4a3d25cb741579444fd6f3459c0e85bb88b6ad9
Daniele <danielebella2000@gmail.com>
2021-07-05 23:13:09 +0100

    Added pin definition header file

---
+++ Firmware/include/pinDefinitions.h
@@ -0,0 +1,11 @@
+// Pin definitions for VLAD
+
+#define BUZZER_PIN 25
+#define SERVO_PIN 33
+#define TWELVE_V_SWITCH 21
+#define SD_CS 15
+#define RADIO_CS 5
+#define RADIO_IRQ 12
+#define RADIO_RESET 14
+#define SDA_PIN 26
+#define SCL_PIN 27
