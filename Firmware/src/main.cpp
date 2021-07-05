525ed36810087d98e4a78845814929ddc99ce2f2
Daniele <danielebella2000@gmail.com>
2021-07-05 21:34:34 +0100

    Added state machine code to main.cpp

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,7 +1,15 @@
-void Setup() {
+#include <Arduino.h>
+#include "stateMachine.h"
+#include "states/preLaunch.h"
 
+stateMachine stateMach;
+
+void Setup() {
+    // Start the state machine with the correct initial state
+    stateMach.initialise(new preLaunch);
 }
 
 void Loop() {
-
+    // Update the state machine
+    stateMach.update();
 }
