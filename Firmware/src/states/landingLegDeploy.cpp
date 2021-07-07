82babbefedcea27bf9c817dc5ded07e2707afeb1
Daniele <danielebella2000@gmail.com>
2021-07-07 19:19:07 +0100

    Added cpp files for the other states

---
+++ Firmware/src/states/landingLegDeploy.cpp
@@ -0,0 +1,13 @@
+#include "landingLegDeploy.h"
+
+void landingLegDeploy::initialise() {
+
+};
+
+State* landingLegDeploy::update() {
+    return this;
+};
+
+void landingLegDeploy::exitState() {
+
+};
