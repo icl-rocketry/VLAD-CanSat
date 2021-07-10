b65ae12d2e9276d1a777ed03d5e3c12734e0f3f8
Daniele <danielebella2000@gmail.com>
2021-07-10 18:09:48 +0100

    Added Spike Code

---
+++ Firmware/src/children/latchControl.cpp
@@ -0,0 +1,63 @@
+#include "latchControl.h"
+
+void latch::setup() {
+    // Set up twelve volt bus switch
+    // IMPORTANT
+    pinMode(TWELVE_V_SWITCH, OUTPUT);
+    digitalWrite(TWELVE_V_SWITCH, LOW);
+    hasRunSetup = true;
+}
+
+void latch::armSpike() {
+    // Set armed flag to true
+    setupCheck();
+    isArmed = true;
+}
+
+
+void latch::beginCountDown() {
+    // Start the countdown for firing the spike
+    setupCheck();
+    if (isArmed) {
+        inCountdown = true;
+        imminentDeployment = true;
+        countdownStartTime = millis();
+    }
+}
+
+void latch::fireSpike() {
+    // Fire the spike
+    // CAUTION: FIRING THE SPIKE IS DANGEROUS
+    setupCheck();
+
+    if (isArmed) {
+        isFiring = true;
+        digitalWrite(TWELVE_V_SWITCH, HIGH);
+        inCountdown = false;
+        imminentDeployment = false;
+        fireStartTime = millis();
+    }
+}
+
+void latch::resetLatch() {
+    // Reset the latch
+    digitalWrite(TWELVE_V_SWITCH, LOW);
+    isFiring = false;
+}
+
+void latch::update() {
+    // Code that checks current time and if state needs updating
+    setupCheck();
+    if (inCountdown && millis() - countdownStartTime >= COUNTDOWN_LEN) {
+        fireSpike();
+    } else if(isFiring && millis() - countdownStartTime >= FIRE_LEN) {
+        resetLatch();
+    }
+}
+
+void latch::setupCheck() {
+    // Check if setup has been run, and runs it if not
+    if (!hasRunSetup) {
+        setup();
+    }
+}
--- Firmware/src/children/latchControl.h
+++ Firmware/src/children/latchControl.h
@@ -1,4 +1,38 @@
 // latch deployment for spike for a short time
 // maybe make way for descent housing
 // WARNINGS: HAS TO BE ARMED AND STUFF FOR SAFETY
-// Victor
+
+#ifndef LATCHCONTROL_H
+#define LATCHCONTROL_H
+
+#define COUNTDOWN_LEN 10000
+#define FIRE_LEN 100
+
+#include <Arduino.h>
+#include <pinDefinitions.h>
+
+class latch {
+    public:
+        void setup(); 
+        void armSpike(); 
+        void beginCountDown();
+        void update();
+
+        bool isArmed; 
+        // Mirror of inCountdown, used to signal to other functions
+        bool imminentDeployment; 
+
+    private:
+        void fireSpike();
+        void resetLatch();
+        void setupCheck();
+
+        bool inCountdown;
+        bool hasRunSetup;
+        bool isFiring;
+
+        uint32_t countdownStartTime;
+        uint32_t fireStartTime;
+};
+
+#endif
